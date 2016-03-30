4d11ab7fe287b82759f933a5bd5e9ba6ab2daed6
Victor Popescu <nomevictor@gmail.com>
2016-03-30 15:32:29 +0300

    Update JS

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,23 +1,27 @@
 
 /*Home page div fade-out effect*/
   
-   /* Check screen size */ 
+   /* Check screen size */
 function detectScreenSize() {
-   if(window.innerWidth <= 750 && window.innerHeight <= 450) {
-     return true;
-   } else {
-     return false;
-   }
-};
+    
+    "use strict";
+    if (window.innerWidth <= 750 && window.innerHeight <= 450) {
+        return true;
+    }
+    
+    else {
+        return false;
+    }
+   
     /*****************/
 
 /*Apply effect only on desktop sized screens*/
 
-if(!detectScreenSize()){
+if (!detectScreenSize()){
     $(window).scroll(function(){
-    var scrollVar = $(window).scrollTop();
-    $('.home-overlay-bg').css("opacity", 1 - scrollVar/700);
-});
-};
+        var scrollVar = $(window).scrollTop();
+        $('.home-overlay-bg').css("opacity", 1 - scrollVar / 700);
+    })
+}
 
 /******end******/
